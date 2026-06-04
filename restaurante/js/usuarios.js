44946b3ffaae376181639b5b5a327ec04b89dac2
juan arenas <juanjac0114@gmail.com>
2026-06-04 16:51:06 -0500

    validate_module_user : add inputs and style

--- restaurante/js/usuarios.js
+++ restaurante/js/usuarios.js
@@ -8,3 +8,27 @@ let cargo  = document.getElementsByName("cargo").Value;
 let fecha_nacimiento = document.getElementById("fecha_nacimiento").Value;
 let correo = document.getElementById("correo").Value;
 let contraseña = document.getElementById("contraseña").Value;
+
+function ValidarDatos(){
+    if(nombre == '' || apellido == '' || numero_documento =='' || telefono== '' || correo == '' ){
+        console.log("los campos estan vacios")
+    }
+    else{
+        if( nombre != /[a-zA_Z]/){
+            console.log('Los datos son incorrectos')
+        }
+        if( apellido != /[a-zA_Z]/){
+            console.log("los datos son incorrectos")
+        }
+        if( numero_documento.length() ==10){
+            console.log("los datos son incorrectos")
+        }
+        if( telefono.length() ==10){
+            console.log("los datos son incorrectos")
+        }
+        if(correo != /[@]/){
+            console.log("los datos son incorrectos")
+        }
+    }
+    
+}
